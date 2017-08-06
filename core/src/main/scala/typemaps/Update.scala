f25989ba0dc143a71738a7a9d82348d90824fe7c
hcoopz <hcoopz@users.noreply.github.com>
2017-08-06 12:37:44 -0400

    Improve update performance
    Add update recursive instances for left-left, left-right, right-left,
    and right-right, and instances for left-head and right-head.

--- core/src/main/scala/typemaps/Update.scala
+++ core/src/main/scala/typemaps/Update.scala
@@ -15,28 +15,57 @@ object Update {
   type Aux[M <: TypeMap, K, A, B, O <: TypeMap] = Update[M, K, A, B] { type Out = O }
   type Monomorphic[M <: TypeMap, K, A] = Update.Aux[M, K, A, A, M]
 
-  implicit def binA[K, A, L <: TypeMap, R <: TypeMap, B]: Update.Aux[Bin[K, A, L, R], K, A, B, Bin[K, B, L, R]] =
+  implicit def head[K, A, L <: TypeMap, R <: TypeMap, B]: Update.Aux[Bin[K, A, L, R], K, A, B, Bin[K, B, L, R]] =
     new Update[Bin[K, A, L, R], K, A, B] {
       override type Out = Bin[K, B, L, R]
       override def apply(m: Bin[K, A, L, R], f: (A) => B): Bin[K, B, L, R] = Bin(f(m.a), m.l, m.r)
       override def const(m: Bin[K, A, L, R], b: B): Bin[K, B, L, R] = Bin(b, m.l, m.r)
     }
 
-  implicit def binL[KA, A, L <: TypeMap, R <: TypeMap, K, AA, B, O <: TypeMap](implicit l: Update.Aux[L, K, AA, B, O]
-                                                                              ): Update.Aux[Bin[KA, A, L, R], K, AA, B, Bin[KA, A, O, R]] =
-    new Update[Bin[KA, A, L, R], K, AA, B] {
-      override type Out = Bin[KA, A, O, R]
-      override def apply(m: Bin[KA, A, L, R], f: (AA) => B): Bin[KA, A, O, R] = Bin(m.a, l(m.l, f), m.r)
-      override def const(m: Bin[KA, A, L, R], b: B): Bin[KA, A, O, R] = Bin(m.a, l.const(m.l, b), m.r)
+  implicit def binLHead[K, A, LK, LA, LL <: TypeMap, LR <: TypeMap, R <: TypeMap, B]: Update.Aux[Bin[K, A, Bin[LK, LA, LL, LR], R], LK, LA, B, Bin[K, A, Bin[LK, B, LL, LR], R]] =
+    new Update[Bin[K, A, Bin[LK, LA, LL, LR], R], LK, LA, B] {
+      override type Out = Bin[K, A, Bin[LK, B, LL, LR], R]
+      override def apply(m: Bin[K, A, Bin[LK, LA, LL, LR], R], f: (LA) => B): Bin[K, A, Bin[LK, B, LL, LR], R] = Bin(m.a, Bin(f(m.l.a), m.l.l, m.l.r), m.r)
+      override def const(m: Bin[K, A, Bin[LK, LA, LL, LR], R], b: B): Bin[K, A, Bin[LK, B, LL, LR], R] = Bin(m.a, Bin(b, m.l.l, m.l.r), m.r)
     }
 
-  // Update in RHS, ensure KB is not equal to KA or present in the LHS
-  implicit def binR[KA, A, L <: TypeMap, R <: TypeMap, K, AA, B, O <: TypeMap](implicit r: Update.Aux[R, K, AA, B, O]
-                                                                              ): Update.Aux[Bin[KA, A, L, R], K, AA, B, Bin[KA, A, L, O]] =
-    new Update[Bin[KA, A, L, R], K, AA, B] {
-      override type Out = Bin[KA, A, L, O]
-      override def apply(m: Bin[KA, A, L, R], f: (AA) => B): Bin[KA, A, L, O] = Bin(m.a, m.l, r(m.r, f))
-      override def const(m: Bin[KA, A, L, R], b: B): Bin[KA, A, L, O] = Bin(m.a, m.l, r.const(m.r, b))
+  implicit def binRHead[K, A, L <: TypeMap, RK, RA, RL <: TypeMap, RR <: TypeMap, B]: Update.Aux[Bin[K, A, L, Bin[RK, RA, RL, RR]], RK, RA, B, Bin[K, A, L, Bin[RK, B, RL, RR]]] =
+    new Update[Bin[K, A, L, Bin[RK, RA, RL, RR]], RK, RA, B] {
+      override type Out = Bin[K, A, L, Bin[RK, B, RL, RR]]
+      override def apply(m: Bin[K, A, L, Bin[RK, RA, RL, RR]], f: (RA) => B): Bin[K, A, L, Bin[RK, B, RL, RR]] = Bin(m.a, m.l, Bin(f(m.r.a), m.r.l, m.r.r))
+      override def const(m: Bin[K, A, L, Bin[RK, RA, RL, RR]], b: B): Bin[K, A, L, Bin[RK, B, RL, RR]] = Bin(m.a, m.l, Bin(b, m.r.l, m.r.r))
+    }
+
+  implicit def binLL[KA, A, LK, LA, LL <: TypeMap, LR <: TypeMap, R <: TypeMap, K, AA, B, O <: TypeMap](implicit ll: Update.Aux[LL, K, AA, B, O]
+                                                                                                       ): Update.Aux[Bin[KA, A, Bin[LK, LA, LL, LR], R], K, AA, B, Bin[KA, A, Bin[LK, LA, O, LR], R]] =
+    new Update[Bin[KA, A, Bin[LK, LA, LL, LR], R], K, AA, B] {
+      override type Out = Bin[KA, A, Bin[LK, LA, O, LR], R]
+      override def apply(m: Bin[KA, A, Bin[LK, LA, LL, LR], R], f: (AA) => B): Bin[KA, A, Bin[LK, LA, O, LR], R] = Bin(m.a, Bin(m.l.a, ll(m.l.l, f), m.l.r), m.r)
+      override def const(m: Bin[KA, A, Bin[LK, LA, LL, LR], R], b: B): Bin[KA, A, Bin[LK, LA, O, LR], R] = Bin(m.a, Bin(m.l.a, ll.const(m.l.l, b), m.l.r), m.r)
+    }
+
+  implicit def binLR[KA, A, LK, LA, LL <: TypeMap, LR <: TypeMap, R <: TypeMap, K, AA, B, O <: TypeMap](implicit lr: Update.Aux[LR, K, AA, B, O]
+                                                                                                       ): Update.Aux[Bin[KA, A, Bin[LK, LA, LL, LR], R], K, AA, B, Bin[KA, A, Bin[LK, LA, LL, O], R]] =
+    new Update[Bin[KA, A, Bin[LK, LA, LL, LR], R], K, AA, B] {
+      override type Out = Bin[KA, A, Bin[LK, LA, LL, O], R]
+      override def apply(m: Bin[KA, A, Bin[LK, LA, LL, LR], R], f: (AA) => B): Bin[KA, A, Bin[LK, LA, LL, O], R] = Bin(m.a, Bin(m.l.a, m.l.l, lr(m.l.r, f)), m.r)
+      override def const(m: Bin[KA, A, Bin[LK, LA, LL, LR], R], b: B): Bin[KA, A, Bin[LK, LA, LL, O], R] = Bin(m.a, Bin(m.l.a, m.l.l, lr.const(m.l.r, b)), m.r)
+    }
+
+  implicit def binRL[KA, A, L <: TypeMap, RK, RA, RL <: TypeMap, RR <: TypeMap, K, AA, B, O <: TypeMap](implicit rl: Update.Aux[RL, K, AA, B, O]
+                                                                                                       ): Update.Aux[Bin[KA, A, L, Bin[RK, RA, RL, RR]], K, AA, B, Bin[KA, A, L, Bin[RK, RA, O, RR]]] =
+    new Update[Bin[KA, A, L, Bin[RK, RA, RL, RR]], K, AA, B] {
+      override type Out = Bin[KA, A, L, Bin[RK, RA, O, RR]]
+      override def apply(m: Bin[KA, A, L, Bin[RK, RA, RL, RR]], f: (AA) => B): Bin[KA, A, L, Bin[RK, RA, O, RR]] = Bin(m.a, m.l, Bin(m.r.a, rl(m.r.l, f), m.r.r))
+      override def const(m: Bin[KA, A, L, Bin[RK, RA, RL, RR]], b: B): Bin[KA, A, L, Bin[RK, RA, O, RR]] = Bin(m.a, m.l, Bin(m.r.a, rl.const(m.r.l, b), m.r.r))
+    }
+
+  implicit def binRR[KA, A, L <: TypeMap, RK, RA, RL <: TypeMap, RR <: TypeMap, K, AA, B, O <: TypeMap](implicit rr: Update.Aux[RR, K, AA, B, O]
+                                                                                                       ): Update.Aux[Bin[KA, A, L, Bin[RK, RA, RL, RR]], K, AA, B, Bin[KA, A, L, Bin[RK, RA, RL, O]]] =
+    new Update[Bin[KA, A, L, Bin[RK, RA, RL, RR]], K, AA, B] {
+      override type Out = Bin[KA, A, L, Bin[RK, RA, RL, O]]
+      override def apply(m: Bin[KA, A, L, Bin[RK, RA, RL, RR]], f: (AA) => B): Bin[KA, A, L, Bin[RK, RA, RL, O]] = Bin(m.a, m.l, Bin(m.r.a, m.r.l, rr(m.r.r, f)))
+      override def const(m: Bin[KA, A, L, Bin[RK, RA, RL, RR]], b: B): Bin[KA, A, L, Bin[RK, RA, RL, O]] = Bin(m.a, m.l, Bin(m.r.a, m.r.l, rr.const(m.r.r, b)))
     }
 
   final class UpdateFn[M <: TypeMap, K, A](val m: M) extends AnyVal {
