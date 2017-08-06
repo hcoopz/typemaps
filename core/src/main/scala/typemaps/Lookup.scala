2e37bc1fcc649e467670f8542bb4db7c3fa1de54
hcoopz <hcoopz@users.noreply.github.com>
2017-08-06 11:23:13 -0400

    Improve type map lookup performance
    Add lookup instances for left-head and right-head.

--- core/src/main/scala/typemaps/Lookup.scala
+++ core/src/main/scala/typemaps/Lookup.scala
@@ -10,7 +10,23 @@ sealed trait Lookup[M <: TypeMap, K] {
   def apply(m: M): Out
 }
 
-object Lookup {
+sealed trait LookupLow {
+  implicit def binL[KA, A, L <: TypeMap, R <: TypeMap, K, O](implicit l: Lookup.Aux[L, K, O]
+                                                            ): Lookup.Aux[Bin[KA, A, L, R], K, O] =
+    new Lookup[Bin[KA, A, L, R], K] {
+      override type Out = O
+      override def apply(m: Bin[KA, A, L, R]): O = l(m.l)
+    }
+
+  implicit def binR[KA, A, L <: TypeMap, R <: TypeMap, K, O](implicit r: Lookup.Aux[R, K, O]
+                                                            ): Lookup.Aux[Bin[KA, A, L, R], K, O] =
+    new Lookup[Bin[KA, A, L, R], K] {
+      override type Out = O
+      override def apply(m: Bin[KA, A, L, R]): O = r(m.r)
+    }
+}
+
+object Lookup extends LookupLow {
   type Aux[M <: TypeMap, K, O] = Lookup[M, K] { type Out = O }
 
   implicit def head[K, A, L <: TypeMap, R <: TypeMap]: Lookup.Aux[Bin[K, A, L, R], K, A] =
@@ -19,17 +35,15 @@ object Lookup {
       override def apply(m: Bin[K, A, L, R]): A = m.a
     }
 
-  implicit def headL[KA, A, L <: TypeMap, R <: TypeMap, K, O](implicit l: Lookup.Aux[L, K, O]
-                                                             ): Lookup.Aux[Bin[KA, A, L, R], K, O] =
-    new Lookup[Bin[KA, A, L, R], K] {
-      override type Out = O
-      override def apply(m: Bin[KA, A, L, R]): O = l(m.l)
+  implicit def binLHead[KA, A, LA, LL <: TypeMap, LR <: TypeMap, R <: TypeMap, K]: Lookup.Aux[Bin[KA, A, Bin[K, LA, LL, LR], R], K, LA] =
+    new Lookup[Bin[KA, A, Bin[K, LA, LL, LR], R], K] {
+      override type Out = LA
+      override def apply(m: Bin[KA, A, Bin[K, LA, LL, LR], R]): LA = m.l.a
     }
 
-  implicit def headR[KA, A, L <: TypeMap, R <: TypeMap, K, O](implicit r: Lookup.Aux[R, K, O]
-                                                             ): Lookup.Aux[Bin[KA, A, L, R], K, O] =
-    new Lookup[Bin[KA, A, L, R], K] {
-      override type Out = O
-      override def apply(m: Bin[KA, A, L, R]): O = r(m.r)
+  implicit def binRHead[KA, A, L <: TypeMap, RA, RL <: TypeMap, RR <: TypeMap, K]: Lookup.Aux[Bin[KA, A, L, Bin[K, RA, RL, RR]], K, RA] =
+    new Lookup[Bin[KA, A, L, Bin[K, RA, RL, RR]], K] {
+      override type Out = RA
+      override def apply(m: Bin[KA, A, L, Bin[K, RA, RL, RR]]): RA = m.r.a
     }
 }
